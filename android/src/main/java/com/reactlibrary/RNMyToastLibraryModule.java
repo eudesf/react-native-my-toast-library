203231a3edfd622a8041b81e120ac44979065a12
Eudes Lima <aeudes@gmail.com>
2019-01-01 19:06:43 -0300

    added missing import

--- android/src/main/java/com/reactlibrary/RNMyToastLibraryModule.java
+++ android/src/main/java/com/reactlibrary/RNMyToastLibraryModule.java
@@ -6,6 +6,7 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.bridge.Callback;
 import android.widget.Toast;
+import java.util.HashMap;
 
 public class RNMyToastLibraryModule extends ReactContextBaseJavaModule {
 
